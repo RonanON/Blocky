25f042064105c152017cd46655f0236f143a9236
RonanON <oneillron@gmail.com>
2014-10-16 18:11:41 +0100

    Some updates

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	private bool ballIsActive;
+	private Vector3 ballPosition;
+	private Vector2 ballInitialForce;
+
+	public GameObject playerObject;
+
+	// Use this for initialization
+	void Start () {
+
+		ballIsActive = false;
+
+		ballInitialForce = new Vector2 (100.0f, 300.0f);
+
+		ballPosition = transform.position;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetButtonDown("Jump") == true){
+
+			if (!ballIsActive){
+
+				rigidbody2D.isKinematic = false;
+				rigidbody2D.AddForce (ballInitialForce);
+				ballIsActive = true;
+
+
+			}
+		
+		}
+
+		if (!ballIsActive && playerObject != null){
+
+			ballPosition.x = playerObject.transform.position.x;
+
+			transform.position = ballPosition;
+
+		
+		}
+
+		if (ballIsActive && transform.position.y < -6) {
+			ballIsActive = !ballIsActive;
+			ballPosition.x = playerObject.transform.position.x;
+			ballPosition.y = -4.2f;
+			transform.position = ballPosition;
+
+			rigidbody2D.isKinematic = true;
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float boundary;
+	public float playerVelocity;
+	private Vector3 playerPosition;
+
+	private int playerLives;
+	private int playerPoints;
+
+	// Use this for initialization
+	void Start () {
+
+		playerPosition = gameObject.transform.position;
+
+		playerLives = 3;
+		playerPoints = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		playerPosition.x += Input.GetAxis ("Horizontal") * playerVelocity;
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+
+			Application.Quit ();
+
+		}
+
+		transform.position = playerPosition;
+
+		if (playerPosition.x < -boundary) {
+			playerPosition.x = -boundary;
+		} 
+		if (playerPosition.x > boundary) {
+			playerPosition.x = boundary;
+		}
+
+		transform.position = playerPosition;
+
+	}
+
+	void addPoints(int points){
+
+		playerPoints += points;
+	}
+
+}
